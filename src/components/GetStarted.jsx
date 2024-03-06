7afdfb74f4cefd6a3451a7424436b0a485d2c93c
Nishchay <nishchay.mago1290@gmail.com>
2024-03-06 19:01:40 +0530

    rightbar init

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,22 +2,22 @@ import GettingStartedVector from "./GettingStartedVector"
 
 export default function GetStarted() {
   return (
-    <div class='text-white bg-blue-700 w-full max-w-sm border border-gray-200 rounded-lg shadow'>
-      <div class='flex flex-col items-center pb-5 px-5 text-center'>
-        <div class='text-xl font-bold max-w-72 p-2 m-2'>
+    <div className='text-white bg-blue-700 w-full max-w-sm border border-gray-200 rounded-lg shadow'>
+      <div className='flex flex-col items-center pb-5 px-5 text-center'>
+        <div className='text-xl font-bold max-w-72 p-2 m-2'>
           Get Started with KoinX for FREE
         </div>
-        <div class='text-md max-w-96 p-1 m-2'>
+        <div className='text-md max-w-96 p-1 m-2'>
           With our range of features that you can equip for free, KoinX allows
           you to be more educated and aware of you tax reports.
         </div>
         <div className='p-1 m-2'>
           <GettingStartedVector />
         </div>
-        <div class='flex items-center justify-between p-1 m-2'>
+        <div className='flex items-center justify-between p-1 m-2'>
           <button
             type='button'
-            class='text-black bg-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2'
+            className='text-black bg-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2'
           >
             <div className='flex items-center'>
               Get Started for FREE
--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,3 +1,72 @@
 export default function Trending() {
-  return <div>trending</div>
+  return (
+    <div className='w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 '>
+      <div className='flex items-center justify-between mb-4'>
+        <h5 className='text-xl font-bold leading-none text-gray-900 '>
+          Trending Coins(24h)
+        </h5>
+      </div>
+      <div className='flow-root'>
+        <ul role='list' className='divide-y divide-gray-200 '>
+          <li className='py-3 sm:py-4'>
+            <div className='flex items-center'>
+              <div className='flex-shrink-0'>
+                <img
+                  className='w-8 h-8 rounded-full'
+                  src='/docs/images/people/profile-picture-1.jpg'
+                  alt='Neil image'
+                />
+              </div>
+              <div className='flex-1 min-w-0 ms-4'>
+                <p className='text-sm font-medium text-gray-900 truncate'>
+                  Neil Sims
+                </p>
+              </div>
+              <div className='inline-flex items-center text-base font-semibold'>
+                $320
+              </div>
+            </div>
+          </li>
+          <li className='py-3 sm:py-4'>
+            <div className='flex items-center '>
+              <div className='flex-shrink-0'>
+                <img
+                  className='w-8 h-8 rounded-full'
+                  src='/docs/images/people/profile-picture-3.jpg'
+                  alt='Bonnie image'
+                />
+              </div>
+              <div className='flex-1 min-w-0 ms-4'>
+                <p className='text-sm font-medium text-gray-900 truncate'>
+                  Bonnie Green
+                </p>
+              </div>
+              <div className='inline-flex items-center text-base font-semibold text-gray-900 '>
+                $3467
+              </div>
+            </div>
+          </li>
+          <li className='py-3 sm:py-4'>
+            <div className='flex items-center'>
+              <div className='flex-shrink-0'>
+                <img
+                  className='w-8 h-8 rounded-full'
+                  src='/docs/images/people/profile-picture-2.jpg'
+                  alt='Michael image'
+                />
+              </div>
+              <div className='flex-1 min-w-0 ms-4'>
+                <p className='text-sm font-medium text-gray-900 truncate'>
+                  Michael Gough
+                </p>
+              </div>
+              <div className='inline-flex items-center text-base font-semibold text-gray-900 '>
+                $67
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
