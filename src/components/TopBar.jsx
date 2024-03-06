bd57ece615964a8b488d4d30c25e23b81389050a
Nishchay <nishchay.mago1290@gmail.com>
2024-03-06 19:01:03 +0530

    improved topbar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,9 +1,9 @@
 export default function TopBar() {
   return (
     <div>
-      <nav class='w-full top-0 bg-white z-10'>
-        <div class='container mx-auto py-5 flex items-center justify-between'>
-          <div class='flex items-center gap-2'>
+      <nav className='w-full top-0 bg-white z-10'>
+        <div className='container mx-auto py-5 flex items-center justify-between'>
+          <div className='flex items-center gap-2'>
             <div className='flex items-center space-x-3 rtl:space-x-reverse'>
               <img
                 src='https://www.koinx.com/_next/static/media/Logo.5f2ad8d5.svg'
@@ -12,34 +12,34 @@ export default function TopBar() {
               />
             </div>
           </div>
-          <ul class='hidden md:flex items-center space-x-10 text-gray-600  font-bold text-sm uppercase'>
-            <li class='hover:text-gray-500'>
+          <ul className='hidden md:flex items-center space-x-10 text-gray-600  font-bold text-sm uppercase'>
+            <li className='hover:text-gray-500'>
               <a href='#'>Crypto</a>
             </li>
-            <li class='hover:text-gray-500'>
+            <li className='hover:text-gray-500'>
               <a href='#'>Free Tools</a>
             </li>
-            <li class='hover:text-gray-500'>
+            <li className='hover:text-gray-500'>
               <a href='#'>Resource Center</a>
             </li>
             <li>
               <button
                 type='button'
-                class='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2'
+                className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2'
               >
                 Get Started
               </button>
             </li>
           </ul>
 
-          <div id='hamburger' class='space-y-1 md:hidden cursor-pointer z-20'>
-            <div class='w-6 h-0.5 bg-black'></div>
-            <div class='w-6 h-0.5 bg-black'></div>
-            <div class='w-6 h-0.5 bg-black'></div>
+          <div id='hamburger' className='space-y-1 md:hidden cursor-pointer z-20'>
+            <div className='w-6 h-0.5 bg-black'></div>
+            <div className='w-6 h-0.5 bg-black'></div>
+            <div className='w-6 h-0.5 bg-black'></div>
           </div>
           <ul
             id='menu'
-            class='hidden absolute left-0 top-0 w-full p-10 rounded-b-3xl space-y-10 text-gray-600 font-bold text-sm uppercase text-center'
+            className='hidden absolute left-0 top-0 w-full p-10 rounded-b-3xl space-y-10 text-gray-600 font-bold text-sm uppercase text-center'
           >
             <li>
               <a id='hLink' href='#'>
