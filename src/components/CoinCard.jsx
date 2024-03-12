aa871a1c0179112cbf539b6ac24d9c4e1b88ef2a
Nishchay <nishchay.mago1290@gmail.com>
2024-03-12 16:03:16 +0530

    changes

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -1,6 +1,17 @@
 import React from "react"
 
 const CoinCard = ({ coin }) => {
+  const bgColorVariants = {
+    red: "bg-red-200",
+    green: "bg-green-200",
+  }
+  const textColorVariants = {
+    red: "text-red-500",
+    green: "text-green-600",
+  }
+  const up = coin.item.data.price_change_percentage_24h.usd > 0
+  const green = "green"
+  const red = "red"
   return (
     <div className='relative w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block rounded-lg overflow-hidden cursor-pointer m-2 outline outline-offset-2 outline-slate-200 p-3 shadow-md'>
       <div className='flex m-2 p-1'>
@@ -10,7 +21,13 @@ const CoinCard = ({ coin }) => {
         />
         <div className='m-1'>{coin.item.symbol}</div>
         <div className='m-1 text-sm'>
-          <span className='bg-red-200 text-red-500'>
+          <span
+            className={
+              up
+                ? `${bgColorVariants[green]} ${textColorVariants[green]}`
+                : `${bgColorVariants[red]} ${textColorVariants[red]}`
+            }
+          >
             {parseFloat(coin.item.data.price_change_percentage_24h.usd).toFixed(
               2
             ) + "%"}
